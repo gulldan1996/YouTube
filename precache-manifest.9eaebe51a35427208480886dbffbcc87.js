a007a1fb02adb665604a62ad1266d20c32a5708d
Olegus <gulldan1996@gmail.com>
2019-07-08 23:22:59 +0300

    Updates

---
+++ precache-manifest.9eaebe51a35427208480886dbffbcc87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e793ec87ec4b69dc041c8cab2d40b0e4",
+    "url": "/YouTube/index.html"
+  },
+  {
+    "revision": "aba77676953a036cbfa2",
+    "url": "/YouTube/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "6fc8d77110b2c16597db",
+    "url": "/YouTube/static/js/2.50ca9e2f.chunk.js"
+  },
+  {
+    "revision": "aba77676953a036cbfa2",
+    "url": "/YouTube/static/js/main.c848193c.chunk.js"
+  },
+  {
+    "revision": "851a7e7d369dcc76551c",
+    "url": "/YouTube/static/js/runtime~main.daa5db3a.js"
+  }
+]);
